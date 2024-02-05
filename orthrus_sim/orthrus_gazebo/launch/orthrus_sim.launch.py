60b3cc46e3fc590ea75fef1c8ce280e440f50796
Evencewu <evencewu@outlook.com>
2024-02-05 19:41:05 +0800

    update

--- orthrus_sim/orthrus_gazebo/launch/orthrus_sim.launch.py
+++ orthrus_sim/orthrus_gazebo/launch/orthrus_sim.launch.py
@@ -1,73 +1,78 @@
-from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+import os
+ 
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-
+ 
 def generate_launch_description():
+
+    model_arg = DeclareLaunchArgument(name='models', description='Absolute path to robot urdf file')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
             PathJoinSubstitution(
-                [FindPackageShare(
-                    'orthrus_gazebo'),
-                    'urdf',
-                    'test_gripper_mimic_joint.xacro.urdf']
+                [FindPackageShare("orthrus_gazebo"), "models", "orthrus", "urdf",  "r2d.urdf.xacro"]
             ),
         ]
     )
-    robot_description = {'robot_description': robot_description_content}
 
-    node_robot_state_publisher = Node(
+    #rivz2
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+    )
+
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+        parameters=[{'robot_description': robot_description_content}],
     )
 
+    # Gazebo launch
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [FindPackageShare('gazebo_ros'), '/launch', '/gazebo.launch.py']
-        ),
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
     )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'gripper'],
-                        output='screen')
-
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
+    #spawn the robot 
+    cranebot_spawn = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=["-topic", "/robot_description", 
+                    "-entity", "r2d",
+                    "-x", '0.0',
+                    "-y", '0.0',
+                    "-z", '1.0']
     )
-
-    load_gripper_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'gripper_controller'],
-        output='screen'
-    )
-
+    
     return LaunchDescription([
-        RegisterEventHandler(
-          event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[load_joint_state_broadcaster],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=load_joint_state_broadcaster,
-                on_exit=[load_gripper_controller],
-            )
-        ),
-        gazebo,
-        node_robot_state_publisher,
-        spawn_entity,
-    ])
+    #rviz2,
+    robot_state_publisher_node,
+    ExecuteProcess(
+        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+        output='screen'),
 
+    ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start','joint_state_broadcaster'],
+        output='screen'
+    ),
+
+    ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start', 'effort_controller'],
+        output='screen'
+    ),
+
+    cranebot_spawn,
+])
