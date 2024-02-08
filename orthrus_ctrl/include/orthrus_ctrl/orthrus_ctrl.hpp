2571f2c97a81efd35b7be5e5b27a581e8524eac9
evencewu <evencewu@outlook.com>
2024-02-09 01:14:45 +0800

    update

--- orthrus_ctrl/include/orthrus_ctrl/orthrus_ctrl.hpp
+++ orthrus_ctrl/include/orthrus_ctrl/orthrus_ctrl.hpp
@@ -1,8 +1,24 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 
+#define PI 3.1415926
+
+#define REAL 0
+#define GAZEBO 1
+#define ISSACSIM 2
+
+#define ISSAC_K_P 10
+#define ISSAC_K_D 1000
+
 namespace othrus_ctrl
 {
+    struct IssacSimJoint
+    {
+        double effort;
+        double position;
+        double velocity;
+    };
+
     class OthrusCtrlNode : public rclcpp::Node
     {
     public:
@@ -10,9 +26,26 @@ namespace othrus_ctrl
         //~OthrusCtrlNode() override;
 
         void main_loop();
-    private:
 
+        // ISSACSIM
+        void IssacSimTorqe();
+
+        // REAL
+
+    private:
+        // ros2
         rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub_;
+        rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
         rclcpp::TimerBase::SharedPtr timer_;
+        sensor_msgs::msg::JointState joint_state_;
+
+        int interfaces_mode_;
+        static const int kJointNum = 12;
+        const std::vector<std::string> joint_name_ = {"hip_LB_joint", "leg1_LB_joint", "leg2_LB_joint",
+                                                      "hip_LF_joint", "leg1_LF_joint", "leg2_LF_joint",
+                                                      "hip_RB_joint", "leg1_RB_joint", "leg2_RB_joint",
+                                                      "hip_RF_joint", "leg1_RF_joint", "leg2_RF_joint"};
+        // ISSACSIM
+        IssacSimJoint IssacSimJoint_[kJointNum];
     };
 }
--- orthrus_ctrl/src/orthrus_ctrl.cpp
+++ orthrus_ctrl/src/orthrus_ctrl.cpp
@@ -1,27 +1,54 @@
 #include "orthrus_ctrl/orthrus_ctrl.hpp"
 
-//
 namespace othrus_ctrl
 {
     OthrusCtrlNode::OthrusCtrlNode() : Node("othrus_ctrl")
     {
-        joint_state_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+        interfaces_mode_ = ISSACSIM;
+
+        joint_state_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_command", 10);
+        //joint_state_sub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(20), std::bind(&OthrusCtrlNode::main_loop, this));
     }
 
     void OthrusCtrlNode::main_loop()
     {
-        // 创建一个joint_state消息
-        sensor_msgs::msg::JointState joint_state;
-        joint_state.name = {"joint1", "joint2", "joint3"};
-        joint_state.position = {0.1, 0.2, 0.3};
-        joint_state.velocity = {0.4, 0.5, 0.6};
-        joint_state.effort = {0.7, 0.8, 0.9};
+        // set null
+        joint_state_.name = joint_name_;
+        joint_state_.position.resize(0);
+        joint_state_.velocity.resize(0);
+        joint_state_.effort.resize(0);
+
+        if (interfaces_mode_ == ISSACSIM)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+               IssacSimJoint_[i].effort = 30;
+            }
+            IssacSimTorqe();
+        }
+        else if (interfaces_mode_ == REAL)
+        {
+            //
+        }
+        else
+        {
+            //
+        }
 
         // 发布消息
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", joint_state.name[0].c_str());
-        joint_state_pub_->publish(joint_state);
+        RCLCPP_INFO(this->get_logger(), "Publish");
+        joint_state_pub_->publish(joint_state_);
+    }
+
+    void OthrusCtrlNode::IssacSimTorqe()
+    {
+        for (int i = 0; i < kJointNum; i++)
+        {
+            joint_state_.position.insert(joint_state_.position.begin(), IssacSimJoint_[i].position - IssacSimJoint_[i].effort / (2 * ISSAC_K_P));
+            joint_state_.velocity.insert(joint_state_.velocity.begin(), IssacSimJoint_[i].velocity - IssacSimJoint_[i].effort / (2 * ISSAC_K_D));
+        }
     }
 }
 
