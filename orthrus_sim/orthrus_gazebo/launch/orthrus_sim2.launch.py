4b36ed86ef493466ef2f11072c025251df2b75f0
Evencewu <evencewu@outlook.com>
2024-02-14 17:40:21 +0800

    update

---
+++ orthrus_sim/orthrus_gazebo/launch/orthrus_sim2.launch.py
@@ -0,0 +1,75 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+             )
+
+    gazebo_ros2_control_demos_path = os.path.join(
+        get_package_share_directory('orthrus_gazebo'))
+
+    xacro_file = os.path.join(gazebo_ros2_control_demos_path,
+                              "models", "orthrus", "urdf",
+                              "orthrus.urdf.xacro")
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'othrus',                  
+                                   '-x', '0.0',
+                                   '-y', '0.0',
+                                   '-z', '1.0'],
+                        output='screen')
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'effort_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ),
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+    ])
--- orthrus_sim/orthrus_gazebo/src/orthrus_gazebo_interface.cpp
+++ orthrus_sim/orthrus_gazebo/src/orthrus_gazebo_interface.cpp
@@ -6,7 +6,7 @@ namespace othrus_gazebo
     {
         joint_torque_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/effort_controller/commands", 10);
         timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(100), std::bind(&OthrusGazeboNode::main_loop, this));
+            std::chrono::milliseconds(10), std::bind(&OthrusGazeboNode::main_loop, this));
     }
 
     void OthrusGazeboNode::main_loop()
@@ -14,10 +14,10 @@ namespace othrus_gazebo
         RCLCPP_INFO(this->get_logger(), "publish\n");
         joint_torque_msg_.data.push_back(0);
 
-        joint_torque_msg_.data[0] = 5;
+        joint_torque_msg_.data[0] = 30;
         joint_torque_pub_->publish(joint_torque_msg_);
     }
-    
+
 }
 
 int main(int argc, char *argv[])
