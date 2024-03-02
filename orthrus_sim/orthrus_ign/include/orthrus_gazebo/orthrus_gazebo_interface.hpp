adfd856d47b8ade8b805100712fc2d5d1be8286f
evencewu <evencewu@outlook.com>
2024-03-02 19:56:33 +0800

    add ign

---
+++ orthrus_sim/orthrus_ign/include/orthrus_gazebo/orthrus_gazebo_interface.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
+
+namespace othrus_gazebo
+{
+    class JointHybrid
+    {
+    public:
+        void Compute(double target_position, double target_velocity, double effort);
+        void SetPd(double k_p, double k_d);
+        void SetParam(double *output, double *position, double *velocity);
+
+        double *output_;
+        double *position_;
+        double *velocity_;
+
+        double k_p_;
+        double k_d_;
+    };
+
+    class OthrusGazeboNode : public rclcpp::Node
+    {
+    public:
+        OthrusGazeboNode();
+
+        void main_loop();
+
+    private:
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr joint_torque_pub_;
+        std_msgs::msg::Float64MultiArray joint_torque_msg_;
+        rclcpp::TimerBase::SharedPtr timer_;
+    };
+}
---
+++ orthrus_sim/orthrus_ign/src/orthrus_gazebo_interface.cpp
@@ -0,0 +1,29 @@
+#include "orthrus_gazebo/orthrus_gazebo_interface.hpp"
+
+namespace othrus_gazebo
+{
+    OthrusGazeboNode::OthrusGazeboNode() : Node("othrus_gazebo")
+    {
+        joint_torque_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/effort_controller/commands", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(10), std::bind(&OthrusGazeboNode::main_loop, this));
+    }
+
+    void OthrusGazeboNode::main_loop()
+    {
+        RCLCPP_INFO(this->get_logger(), "publish\n");
+        joint_torque_msg_.data.push_back(0);
+
+        joint_torque_msg_.data[0] = 30;
+        joint_torque_pub_->publish(joint_torque_msg_);
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<othrus_gazebo::OthrusGazeboNode>());
+    rclcpp::shutdown();
+    return 0;
+}
