cdbcf4f626db4bd9296ea1ce33045e5e3f5a9e18
evence <evencewu@outlook.com>
2024-03-04 12:21:44 +0800

    update

--- orthrus_sim/orthrus_gazebo/src/orthrus_gazebo_interface.cpp
+++ orthrus_sim/orthrus_gazebo/src/orthrus_gazebo_interface.cpp
@@ -6,15 +6,18 @@ namespace othrus_gazebo
     {
         joint_torque_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/effort_controller/commands", 10);
         timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(10), std::bind(&OthrusGazeboNode::main_loop, this));
+            std::chrono::milliseconds(1), std::bind(&OthrusGazeboNode::main_loop, this));
     }
 
     void OthrusGazeboNode::main_loop()
     {
         RCLCPP_INFO(this->get_logger(), "publish\n");
-        joint_torque_msg_.data.push_back(0);
+        joint_torque_msg_.layout.dim.push_back(std_msgs::msg::MultiArrayDimension());
+        joint_torque_msg_.layout.dim[0].size = 12;
 
-        joint_torque_msg_.data[0] = 30;
+        std::vector<double> data {-30, -30, -30, -30, -30, -30, -30, -30, -30, -30, -30, -30};
+        joint_torque_msg_.data = data;
+
         joint_torque_pub_->publish(joint_torque_msg_);
     }
 
