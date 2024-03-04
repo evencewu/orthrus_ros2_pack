227f902113c5df9fcb37ed301142dd4e1e86c58a
evence <evencewu@outlook.com>
2024-03-04 15:07:05 +0800

    update

--- orthrus_sim/orthrus_gazebo/include/orthrus_gazebo/orthrus_gazebo_interface.hpp
+++ orthrus_sim/orthrus_gazebo/include/orthrus_gazebo/orthrus_gazebo_interface.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
+
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <orthrus_interfaces/msg/ctrl_cmd.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
 namespace othrus_gazebo
 {
+    struct JointParam
+    {
+        std::string name;
+
+        double position;
+        double velocity;
+        double effort;
+    };
+
     class JointHybrid
     {
     public:
@@ -28,19 +39,24 @@ namespace othrus_gazebo
 
         void main_loop();
 
-        
-
     private:
-        void CtrlCmdCallback(const orthrus_interfaces::msg::CtrlCmd::SharedPtr msg);
-
         rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr joint_torque_pub_;
         std_msgs::msg::Float64MultiArray joint_torque_msg_;
-        rclcpp::TimerBase::SharedPtr timer_;
 
-        rclcpp::Subscription<orthrus_interfaces::msg::CtrlCmd>::SharedPtr ctrl_cmd_pub_;
+        rclcpp::Subscription<orthrus_interfaces::msg::CtrlCmd>::SharedPtr ctrl_cmd_sub_;
         orthrus_interfaces::msg::CtrlCmd ctrl_cmd_msg_;
 
-        double hybrid_[12];
-        
+        void CtrlCmdCallback(const orthrus_interfaces::msg::CtrlCmd::SharedPtr msg);
+
+        rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
+        sensor_msgs::msg::JointState joint_state_msg_;
+
+        void JointStateCallback(const sensor_msgs::msg::JointState::SharedPtr msg);
+
+        std::vector<double> hybrid_output_ = std::vector<double>(12);
+
+        JointParam joint_param_[12];
+
+        std::vector<std::string> joint_name_{"hip_RF_joint", "leg1_RF_joint", "leg2_RF_joint", "hip_LF_joint", "leg1_LF_joint", "leg2_LF_joint", "hip_RB_joint", "leg1_RB_joint", "leg2_RB_joint", "hip_LB_joint", "leg1_LB_joint", "leg2_LB_joint"};
     };
 }
--- orthrus_sim/orthrus_gazebo/src/orthrus_gazebo_interface.cpp
+++ orthrus_sim/orthrus_gazebo/src/orthrus_gazebo_interface.cpp
@@ -4,28 +4,51 @@ namespace othrus_gazebo
 {
     OthrusGazeboNode::OthrusGazeboNode() : Node("othrus_gazebo")
     {
+        for (int i = 0; i < 12; i++)
+        {
+            joint_param_[i].name = joint_name_[i];
+        }
+
         joint_torque_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/effort_controller/commands", 10);
 
-        ctrl_cmd_pub_ = this->create_subscription<orthrus_interfaces::msg::CtrlCmd>("/orthrus_cmd", 10,
-            std::bind(&OthrusGazeboNode::CtrlCmdCallback, this, std::placeholders::_1));
+        ctrl_cmd_sub_ = this->create_subscription<orthrus_interfaces::msg::CtrlCmd>("/orthrus_cmd", 10,
+                                                                                    std::bind(&OthrusGazeboNode::CtrlCmdCallback, this, std::placeholders::_1));
+
+        joint_state_sub_ = this->create_subscription<sensor_msgs::msg::JointState>("/joint_states", 10,
+                                                                                   std::bind(&OthrusGazeboNode::JointStateCallback, this, std::placeholders::_1));
     }
 
     void OthrusGazeboNode::CtrlCmdCallback(const orthrus_interfaces::msg::CtrlCmd::SharedPtr msg)
     {
-        for(int i = 0;i<12;i++)
+        for (int i = 0; i < 12; i++)
         {
-            hybrid_[i] = msg->motor_cmd[i].torqe;
+            hybrid_output_[i] = msg->motor_cmd[i].k_p * (msg->motor_cmd[i].target_p - joint_param_[i].position) - msg->motor_cmd[i].k_d * (msg->motor_cmd[i].target_d - joint_param_[i].velocity) + msg->motor_cmd[i].torqe;
         }
 
-
-        RCLCPP_INFO(this->get_logger(), "publish\n");
+        
         joint_torque_msg_.layout.dim.push_back(std_msgs::msg::MultiArrayDimension());
         joint_torque_msg_.layout.dim[0].size = 12;
 
-        std::vector<double> output_data {-30, -30, -30, -30, -30, -30, -30, -30, -30, -30, -30, -30};
-        joint_torque_msg_.data = data;
+        joint_torque_msg_.data = hybrid_output_;
 
         joint_torque_pub_->publish(joint_torque_msg_);
+    }
+
+    void OthrusGazeboNode::JointStateCallback(const sensor_msgs::msg::JointState::SharedPtr msg)
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                if (msg->name[i] == joint_param_[j].name)
+                {
+                    joint_param_[j].position = msg->position[i];
+                    joint_param_[j].velocity = msg->velocity[i];
+                    joint_param_[j].effort = msg->effort[i];
+                    //RCLCPP_INFO(this->get_logger(), "find_name\n");
+                }
+            }
+        }
     }
 }
 
