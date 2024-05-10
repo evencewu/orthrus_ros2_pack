58772e3cbe398fc58b36e55f30b901077440a8ca
evencewu <evencewu@outlook.com>
2024-05-10 21:31:46 +0800

    update

---
+++ orthrus_controllers/src/OrthrusController.cpp
@@ -0,0 +1,5 @@
+#include "orthrus_controllers/OrthrusController.hpp"
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(my_controllers::MyController, controller_interface::ControllerInterface)
